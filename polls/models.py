864ff8c4da4d50d8ebd578c3dc0a655980453e07
trakerzod1337 <pavlego@yandex.ru>
2025-10-07 01:57:54 +0300

    first initial

---
+++ polls/models.py
@@ -0,0 +1,45 @@
+import datetime
+from django.contrib import admin
+
+from django.db import models
+from django.utils import timezone
+
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+
+    @property
+    def total_votes(self):
+        return sum(choice.votes for choice in self.choice_set.all())
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+    @property
+    def percentage(self):
+        total = self.question.total_votes
+        if total > 0:
+            return (self.votes / total) * 100
+        return 0
