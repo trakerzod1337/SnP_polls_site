660c969e2e96ef49c98521d2855f101a206b2b5a
trakerzod1337 <pavlego@yandex.ru>
2025-10-07 13:24:07 +0300

    Fix static files configuration

--- SnP_site_task/settings.py
+++ SnP_site_task/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=0s7k0h8d%z%+7z3iqq$=bnj@4k&d7b7qc-ar)y533z*_b&%rb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['https://snp-polls-site.onrender.com/']
 
 
 # Application definition
